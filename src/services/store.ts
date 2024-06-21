bdcab1256dff0bc4f43438c905e667c20ffcd299
Arctic Fox <tyskanhik1@mail.ru>
2024-06-21 12:34:39 +0300

    create orders

--- src/services/store.ts
+++ src/services/store.ts
@@ -10,13 +10,15 @@ import { userSlice } from './slices/user';
 import { burgerConstructorSlice } from './slices/burgerConstructor';
 import { feedSlice } from './slices/feed';
 import { orderSlice } from './slices/order';
+import { ordersSlice } from './slices/ordes';
 
 const rootReducer = combineReducers({
   [ingredientSlice.name]: ingredientSlice.reducer,
   [userSlice.name]: userSlice.reducer,
   [burgerConstructorSlice.name]: burgerConstructorSlice.reducer,
   [feedSlice.name]: feedSlice.reducer,
-  [orderSlice.name]: orderSlice.reducer
+  [orderSlice.name]: orderSlice.reducer,
+  [ordersSlice.name]: ordersSlice.reducer
 });
 
 const store = configureStore({
